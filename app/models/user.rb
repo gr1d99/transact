e8e5779baaae84bb24212ba21bc17b29b4c0044e
Gideon Kimutai <gideon.kimutai.kim@gmail.com>
2021-05-31 09:15:41 +0300

    chore(users): add tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  with_options presence: true do
+    validates :email, uniqueness: true
+    validates :phone, uniqueness: true
+    validates :first_name
+    validates :last_name
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Validations' do
+    specify { should validate_presence_of(:email) }
+    specify { should validate_uniqueness_of(:email) }
+    specify { should validate_presence_of(:phone) }
+    specify { should validate_uniqueness_of(:phone) }
+    specify { should validate_presence_of(:first_name) }
+    specify { should validate_presence_of(:last_name) }
+  end
+
+  context 'Associations' do
+  end
 end
