4e5690a96852227709d3d608cbf1fbb952d9b246
Gideon Kimutai <gideon.kimutai.kim@gmail.com>
2021-06-16 08:09:30 +0300

    chore: update host

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,6 @@ module Transact
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.hosts << 'ec2-35-158-138-163.eu-central-1.compute.amazonaws.com'
+    config.hosts << "gr1d99.co.ke"
   end
 end
