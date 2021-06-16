b3ab4f83992570bcd5a3fbb3c8d75f3c64de6e22
Gideon Kimutai <gideon.kimutai.kim@gmail.com>
2021-06-16 07:55:32 +0300

    chore: add allowed hosts

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,6 @@ module Transact
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.hosts << 'ec2-35-158-138-163.eu-central-1.compute.amazonaws.com'
   end
 end
